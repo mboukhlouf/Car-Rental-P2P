54df8fcd2ebdec7dfd64b1d3bb7bb2131d85d38c
c.kawtar1998 <c.kawtar1998@gmail.com>
2020-01-08 16:43:52 +0100

    reservation request

---
+++ LocationDeVoitures/ViewModels/ReservationDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LocationDeVoitures.Models.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationDeVoitures.ViewModels
+{
+    public class ReservationDetailsViewModel : BaseViewModel
+    {
+        public Advertisement Advertisement { get; set; }
+
+        public ReservationViewModel ReservationViewModel { get; set; }
+    }
+}
